7bb95e158630e8ebc2147b2c5e03273191216ee7
ktyudha <ktyudha@gmail.com>
2025-02-24 08:41:27 +0700

    feat(user): update slider prev next methods

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,3 +1,4 @@
+import { ReactElement, useRef } from "react";
 import clsx from "clsx";
 import SlickSlider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -12,6 +13,10 @@ type SlideProps = {
   toShow?: number;
   className?: string;
   centerMode?: boolean;
+  nextArrow?: ReactElement;
+  prevArrow?: ReactElement;
+  isAutoplay?: boolean;
+  autoplaySpeed?: number;
 };
 
 export default function Slider({
@@ -21,7 +26,13 @@ export default function Slider({
   centerMode,
   dots,
   dotsPosition,
+  isAutoplay,
+  autoplaySpeed,
+  nextArrow,
+  prevArrow,
 }: SlideProps) {
+  const sliderRef = useRef<SlickSlider | null>(null);
+
   const settings = {
     centerMode: centerMode || false,
     dots: dots ?? true,
@@ -30,16 +41,41 @@ export default function Slider({
     slidesToShow: toShow || 1,
     slidesToScroll: 1,
     arrows: false,
+    autoplay: isAutoplay || false,
+    autoplaySpeed: isAutoplay ? autoplaySpeed : 0,
     focusOnSelect: false,
+    nextArrow: nextArrow,
+    prevArrow: prevArrow,
+  };
+
+  const next = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickNext();
+    }
+  };
+
+  const previous = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickPrev();
+    }
   };
 
   return (
-    <SlickSlider {...settings} className={clsx([dotsPosition || "left"])}>
-      {images?.map((image, index) => (
-        <div key={index}>
-          <Image name={image} className={clsx([className])} />
-        </div>
-      ))}
-    </SlickSlider>
+    <div className="slider-container">
+      <SlickSlider
+        ref={sliderRef}
+        {...settings}
+        className={clsx([dotsPosition || "left"])}
+      >
+        {images?.map((image, index) => (
+          <div key={index}>
+            <Image name={image} className={clsx([className])} />
+          </div>
+        ))}
+      </SlickSlider>
+
+      {nextArrow && <div onClick={next}>{nextArrow}</div>}
+      {prevArrow && <div onClick={previous}>{prevArrow}</div>}
+    </div>
   );
 }
--- src/modules/user/home/views/_sections/SliderSection.tsx
+++ src/modules/user/home/views/_sections/SliderSection.tsx
@@ -3,15 +3,61 @@ import Slider from "@components/Slider/Slider";
 export default function SliderSection() {
   const imagesSlider = ["slider-1", "slider-2", "slider-3"];
 
+  const PrevArrow = (props: any) => {
+    const { onClick } = props;
+    return (
+      <button
+        className="absolute lg:top-1/2 bottom-0 lg:left-24 md:left-16 left-2 -translate-y-1/2 px-4 py-2 rounded cursor-pointer"
+        onClick={onClick}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          className="bi bi-arrow-left h-6 w-6 text-gray-300 hover:text-white ease-in-out duration-300"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+          />
+        </svg>
+      </button>
+    );
+  };
+
+  const NextArrow = (props: any) => {
+    const { onClick } = props;
+    return (
+      <button
+        type="button"
+        className="absolute lg:top-1/2 bottom-0 lg:right-24 md:right-16 right-2 -translate-y-1/2 px-4 py-2 rounded cursor-pointer"
+        onClick={onClick}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          className="bi bi-arrow-right h-6 w-6 text-gray-300 hover:text-white ease-in-out duration-300"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+          />
+        </svg>
+      </button>
+    );
+  };
+
   return (
     <>
-      <section className="relative">
+      <section className="relative" id="home">
         <Slider
           dots={false}
           images={imagesSlider}
-          className="h-[78vh] w-full object-cover object-center z-50"
+          prevArrow={<PrevArrow />}
+          nextArrow={<NextArrow />}
+          className="h-[78vh] w-full object-cover object-center z-50 brightness-75"
         />
-        <div className="absolute h-[78vh] inset-0 bg-black opacity-30 z-10 pointer-events-none"></div>
       </section>
     </>
   );
