d3a93b60f6b6ae3b6cbc8978f40a80afeb5b7151
ktyudha <ktyudha@gmail.com>
2025-09-29 21:29:56 +0700

    feat: add service for invoice

--- src/services/admin/invoice/hooks/useUpdate.ts
+++ src/services/admin/invoice/hooks/useUpdate.ts
@@ -1,35 +1,41 @@
-// import axiosInstance from "@/lib/axios-instance";
-// import useRevalidateMutation from "@/lib/swr/useRevalidateMutation";
-// import { ICreatePayload } from "../interfaces/create.type";
+import axiosInstance from "@/lib/axios-instance";
+import useRevalidateMutation from "@/lib/swr/useRevalidateMutation";
+import { ICreatePayload } from "../interfaces/create.type";
+import { jsonToFormData, JSONValue } from "@helpers/json-to-form-data";
 
-// export default function useUpdate(packageAddOnId: string) {
-//   const revalidateMutationsByKey = useRevalidateMutation();
+export default function useUpdate(invoiceId: string) {
+  const revalidateMutationsByKey = useRevalidateMutation();
 
-//   const updateData = async (payload: ICreatePayload) => {
-//     const { name, price } = payload;
-//     try {
-//       const res = await axiosInstance({
-//         withToken: true,
-//         tokenType: "admin",
-//       }).post(`/admin/package-addons/${packageAddOnId}`, {
-//         name,
-//         price,
-//         _method: "PUT",
-//       });
+  const updateData = async (payload: ICreatePayload) => {
+    try {
+      const res = await axiosInstance({
+        withToken: true,
+        tokenType: "admin",
+      }).post(
+        `/admin/invoices/${invoiceId}`,
+        jsonToFormData({ ...payload, _method: "PUT" } as unknown as {
+          [key: string]: JSONValue;
+        }),
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
 
-//       if (res.status === 200) {
-//         revalidateMutationsByKey(/^\/admin\/package-addons/);
-//       }
+      if (res.status === 200) {
+        revalidateMutationsByKey(/^\/admin\/invoices/);
+      }
 
-//       return { response: res, error: null };
-//     } catch (error: any) {
-//       if (error.status >= 500) {
-//         return { response: null, error: "Server error" };
-//       }
+      return { response: res, error: null };
+    } catch (error: any) {
+      if (error.status >= 500) {
+        return { response: null, error: "Server error" };
+      }
 
-//       return { response: null, error: error.data.message };
-//     }
-//   };
+      return { response: null, error: error.data.message };
+    }
+  };
 
-//   return { updateData };
-// }
+  return { updateData };
+}
